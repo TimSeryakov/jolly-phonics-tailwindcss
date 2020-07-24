3214993e7bf01b0963f8785aac01c8282be5a9c9
Tim Seryakov <t.seryakov@gmail.com>
2020-07-24 15:49:32 +0300

    almost done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,49 @@
 new WOW(
   {
     offset: 0,
-    mobile: false,
+    mobile: true,
     resetAnimation: false,
   }
 ).init();
 
 
-var emblaNode = document.getElementById("embla");
-var embla = EmblaCarousel(emblaNode);
+
+const siemaOne = new Siema({
+  selector: '.siemaOne',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: {
+    100: 1.2,
+    640: 2.2,
+    768: 2.3,
+    1024: 3,
+  },
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
+
+const siemaTwo = new Siema({
+  selector: '.siemaTwo',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: {
+    100: 1.2,
+    640: 2.2,
+    768: 2.3,
+    1024: 3,
+  },
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
